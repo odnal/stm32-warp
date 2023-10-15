f4a79723ba030eb0f9dbb2e7a7e88c6c8810664d
landonbr <landonbrown0797@gmail.com>
2023-10-15 19:15:14 -0400

    update

--- blink2/prog.s
+++ blink2/prog.s
@@ -8,12 +8,11 @@ prog:
   ldr r0, =RCC_BASE
   mov r1, #0x4C         // RCC_AHB2ENR offset
   orr r0, r1
-  //str r0, [r0]
   
   // Enable GPIOE in the RCC_AHB2ENR register
   mov r1, #1
   lsl r1, r1, #4        // GPIOE port enable bit
-  str r1, [r0]          // store value in RCC_AHB2ENR 
+  str r1, [r0]          // store value in RCC_AHB2ENR register
 
   // Set up GPIO_MODER register to general purpose output mode: 01
   ldr r0, =GPIOE_BASE   // MODER register has offset 0x00
@@ -25,7 +24,6 @@ prog:
   
   mov r1, #0x18         // GPIOE_BSRR offset value 
   orr r0, r1            // GPIOE_BSRR address is now contained in r0
-  //str r0, [r0]
 
   led_loop:
     mov r1, #1
@@ -45,16 +43,8 @@ prog:
     sub r3, #1
     bne delay
     bx lr
-    
-
-
-
-.thumb_func
-forever:
-  b .
 
 .section .data
   .equ RCC_BASE,    0x40021000 
   .equ GPIOE_BASE,  0x48001000
-
-  .equ counter,     0x00200000 
+  .equ counter,     0x00200000
