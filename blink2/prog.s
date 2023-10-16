594d84072b6c8b979db05fe4b98d64404a6e265f
landonbr <landonbrown0797@gmail.com>
2023-10-15 20:02:10 -0400

    update

--- blink2/prog.s
+++ blink2/prog.s
@@ -6,21 +6,17 @@
 prog:
   // Set up GPIO clock bus (RCC_AHB2ENR)
   ldr r0, =RCC_BASE
-  mov r1, #0x4C         // RCC_AHB2ENR offset
-  orr r0, r1
-  
-  // Enable GPIOE in the RCC_AHB2ENR register
   mov r1, #1
   lsl r1, r1, #4        // GPIOE port enable bit
-  str r1, [r0]          // store value in RCC_AHB2ENR register
+  str r1, [r0, #0x4C]   // Store result in the RCC_AHB2ENR register
 
   // Set up GPIO_MODER register to general purpose output mode: 01
   ldr r0, =GPIOE_BASE   // MODER register has offset 0x00
   mov r1, #1
   lsl r1, r1, #17       // the bit we want to change
-  ldr r2, [r0]          // r2 has reset value of 0xffffffff
+  ldr r2, [r0, #0]      // r2 has reset value of 0xffffffff
   eor r2, r1            // value changed to output mode for GPIOE
-  str r2, [r0]          // store result to GPIOE_MODER (address)
+  str r2, [r0, #0]      // store result to GPIOE_MODER (address)
   
   mov r1, #0x18         // GPIOE_BSRR offset value 
   orr r0, r1            // GPIOE_BSRR address is now contained in r0
